b1ffcd04076a12539035b814dee9b793e33ba440
kuribayashi1026 <e1b18032@st.oit.ac.jp>
2020-10-27 15:11:46 +0900

    aa

---
+++ src/main/java/moblec05/database5/Database5Application.java
@@ -0,0 +1,13 @@
+package moblec05.database5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Database5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Database5Application.class, args);
+	}
+
+}
---
+++ src/test/java/moblec05/database5/Database5ApplicationTests.java
@@ -0,0 +1,13 @@
+package moblec05.database5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Database5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
